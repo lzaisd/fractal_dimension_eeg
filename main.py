eea27f9f0105fbeb0456c55256e0087fd84df294
Elizaveta Sidorova <elizavetka.sidorova@bk.ru>
2023-05-02 22:04:12 +0300

    adding wievlet

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import filedialog
+from scipy.signal import decimate
 
 data = np.genfromtxt('file.asc', delimiter='\n')
 # размер участка
+# размер участка
 window_size = 10000
-# разбить сигнал на участки
-windows = [data[i:i+window_size] for i in range(0, len(data), window_size)]
-# вычислить дисперсию для каждого участка
-variances = [np.var(window) for window in windows]
+
+# Perform the DWT
+wavelet = 'db4'  # Adjust the wavelet as needed
+level = 4  # Adjust the decomposition level as needed
+coeffs = [data]
+for _ in range(level):
+    cA, cD = decimate(coeffs[-1], 2, zero_phase=True), np.zeros_like(coeffs[-1])
+    cD[::2] = cA
+    coeffs.append(cD)
+
+# Calculate the wavelet spectrum
+wavelet_spectrum = [np.sum(np.square(c)) for c in coeffs]
+
 # размеры участков
-sizes = [len(window) for window in windows]
-# построить график зависимости логарифма дисперсии от логарифма размера участков
-# plt.plot(np.log(sizes), np.log1p(variances))
-# plt.xlabel('Log(Window size)')
-# plt.ylabel('Log(Variance)')
-# plt.show()
+sizes = [len(data) // (2 ** i) for i in range(level+1)]
 # найти угол наклона графика
-slope, intercept = np.polyfit(np.log(sizes), np.log1p(variances), 1)
+# найти угол наклона графика
+slope, intercept = np.polyfit(np.log(sizes), np.log(wavelet_spectrum), 1)
 # коэффициент самоподобия
 fractal_dimension = slope
 root = tk.Tk()
@@ -36,16 +43,14 @@ fig.suptitle('Коэффициент самоподобия ЭЭГ')
 
 # построить график ЭЭГ
 axs[0].plot(data)
-axs[0].set_xlim([0, 214000])
+axs[0].set_xlim([5000, 214000])
 # построить график зависимости логарифма дисперсии от логарифма размера участков
-window_size = 10000
-windows = [data[i:i+window_size] for i in range(0, len(data), window_size)]
-sizes = [len(window) for window in windows]
-variances = [np.var(window) for window in windows]
-axs[1].plot(np.log(sizes), np.log1p(variances))
+axs[1].plot(np.log(sizes), np.log(wavelet_spectrum))
+axs[1].plot(np.log(sizes), slope * np.log(sizes) + intercept, color='red', label='Аппроксимирующая линия')
+axs[1].set_xlim([9.5, 12.274])
 axs[1].set_xlabel('Log(размера окна)')
-axs[1].set_ylabel('Log(дисперсии)')
+axs[1].set_ylabel('Log(вейвлет спектра)')
 axs[1].set_title('Коэффициент самоподобия = ' + str(round(fractal_dimension, 6)))
-axs[1].set_xlim([8.292, 9.190])
+axs[1].legend()
 
 plt.show()
